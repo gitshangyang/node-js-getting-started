354afc5f4c7cc8107508c594c40c018839b444c6
gitshangyang <64315449+gitshangyang@users.noreply.github.com>
2020-07-31 23:29:20 +0200

    s

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var express = require("express");
 var app = express();
+const PORT = process.env.PORT || 5000
 
-app.listen(3000, () => {
+
+app.listen(PORT, () => {
  console.log("Server running on port 3000");
 });
 
