11a57aca1bffdc284755d359de32621e2d71d722
gitshangyang <64315449+gitshangyang@users.noreply.github.com>
2020-08-17 18:57:35 +0200

    ss

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ var express = require("express");
 var app = express();
 const PORT = process.env.PORT || 5000
 
+function sleep(ms) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+  } 
 
 app.listen(PORT, () => {
  console.log("Server running on port 3000");
@@ -13,8 +18,3 @@ app.get("/url", (req, res, next) => {
     await sleep(5000);
    });
 
-   function sleep(ms) {
-    return new Promise((resolve) => {
-      setTimeout(resolve, ms);
-    });
-  } 
