4115bb07131231e684b7c1da5d465fc016488779
gitshangyang <64315449+gitshangyang@users.noreply.github.com>
2020-08-17 19:19:23 +0200

    ss

--- app.js
+++ app.js
@@ -9,12 +9,13 @@ app.listen(PORT, () => {
 });
 
 app.get("/url", (req, res, next) => {
+    sleepFor(1000);
     res.setTimeout(3000);
-    sleepFor(5000);
+    sleepFor(10000);
     res.json(["Tony","Lissa","Michael","Ginger","Food"]);
    });
 
    function sleepFor( sleepDuration ){
     var now = new Date().getTime();
-    while(new Date().getTime() < now + sleepDuration){ /* do nothing */ } 
+    while(new Date().getTime() < now + sleepDuration){ console.log('server execution time:' sleepDuration) } 
 }
