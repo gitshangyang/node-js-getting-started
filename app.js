8b1f8d571090be1ec7935e092a79863788c8ccaa
gitshangyang <64315449+gitshangyang@users.noreply.github.com>
2020-08-17 18:54:37 +0200

    ss

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
  console.log("Server running on port 3000");
- await sleep(5000);
+ 
 });
 
 app.get("/url", (req, res, next) => {
     res.json(["Tony","Lissa","Michael","Ginger","Food"]);
+    await sleep(5000);
    });
