8370d2a83abad356030ad427a43c1ed684ed0392
gitshangyang <64315449+gitshangyang@users.noreply.github.com>
2020-12-20 19:36:29 +0100

    ss

--- app.js
+++ app.js
@@ -8,7 +8,137 @@ app.listen(PORT, () => {
  
 });
 
-app.get("/url", (req, res, next) => {
+app.get("/url1", (req, res, next) => {
+    //sleepFor(1000);
+    //console.log('server execution time:1000 ');
+    //res.setTimeout(3000);
+    sleepFor(10000);
+    console.log('server execution time: 20000');
+    res.json(["Tony","Lissa","Michael","Ginger","Food"]);
+   });
+
+   function sleepFor( sleepDuration ){
+    var now = new Date().getTime();
+    while(new Date().getTime() < now + sleepDuration){  } 
+}
+app.get("/url2", (req, res, next) => {
+    //sleepFor(1000);
+    //console.log('server execution time:1000 ');
+    //res.setTimeout(3000);
+    sleepFor(10000);
+    console.log('server execution time: 20000');
+    res.json(["Tony","Lissa","Michael","Ginger","Food"]);
+   });
+
+   function sleepFor( sleepDuration ){
+    var now = new Date().getTime();
+    while(new Date().getTime() < now + sleepDuration){  } 
+}
+app.get("/url3", (req, res, next) => {
+    //sleepFor(1000);
+    //console.log('server execution time:1000 ');
+    //res.setTimeout(3000);
+    sleepFor(10000);
+    console.log('server execution time: 20000');
+    res.json(["Tony","Lissa","Michael","Ginger","Food"]);
+   });
+
+   function sleepFor( sleepDuration ){
+    var now = new Date().getTime();
+    while(new Date().getTime() < now + sleepDuration){  } 
+}
+app.get("/url4", (req, res, next) => {
+    //sleepFor(1000);
+    //console.log('server execution time:1000 ');
+    //res.setTimeout(3000);
+    sleepFor(10000);
+    console.log('server execution time: 20000');
+    res.json(["Tony","Lissa","Michael","Ginger","Food"]);
+   });
+
+   function sleepFor( sleepDuration ){
+    var now = new Date().getTime();
+    while(new Date().getTime() < now + sleepDuration){  } 
+}
+app.get("/url5", (req, res, next) => {
+    //sleepFor(1000);
+    //console.log('server execution time:1000 ');
+    //res.setTimeout(3000);
+    sleepFor(10000);
+    console.log('server execution time: 20000');
+    res.json(["Tony","Lissa","Michael","Ginger","Food"]);
+   });
+
+   function sleepFor( sleepDuration ){
+    var now = new Date().getTime();
+    while(new Date().getTime() < now + sleepDuration){  } 
+}
+app.get("/url6", (req, res, next) => {
+    //sleepFor(1000);
+    //console.log('server execution time:1000 ');
+    //res.setTimeout(3000);
+    sleepFor(10000);
+    console.log('server execution time: 20000');
+    res.json(["Tony","Lissa","Michael","Ginger","Food"]);
+   });
+
+   function sleepFor( sleepDuration ){
+    var now = new Date().getTime();
+    while(new Date().getTime() < now + sleepDuration){  } 
+}
+app.get("/url7", (req, res, next) => {
+    //sleepFor(1000);
+    //console.log('server execution time:1000 ');
+    //res.setTimeout(3000);
+    sleepFor(10000);
+    console.log('server execution time: 20000');
+    res.json(["Tony","Lissa","Michael","Ginger","Food"]);
+   });
+
+   function sleepFor( sleepDuration ){
+    var now = new Date().getTime();
+    while(new Date().getTime() < now + sleepDuration){  } 
+}
+app.get("/url8", (req, res, next) => {
+    //sleepFor(1000);
+    //console.log('server execution time:1000 ');
+    //res.setTimeout(3000);
+    sleepFor(10000);
+    console.log('server execution time: 20000');
+    res.json(["Tony","Lissa","Michael","Ginger","Food"]);
+   });
+
+   function sleepFor( sleepDuration ){
+    var now = new Date().getTime();
+    while(new Date().getTime() < now + sleepDuration){  } 
+}
+app.get("/url9", (req, res, next) => {
+    //sleepFor(1000);
+    //console.log('server execution time:1000 ');
+    //res.setTimeout(3000);
+    sleepFor(10000);
+    console.log('server execution time: 20000');
+    res.json(["Tony","Lissa","Michael","Ginger","Food"]);
+   });
+
+   function sleepFor( sleepDuration ){
+    var now = new Date().getTime();
+    while(new Date().getTime() < now + sleepDuration){  } 
+}
+app.get("/url10", (req, res, next) => {
+    //sleepFor(1000);
+    //console.log('server execution time:1000 ');
+    //res.setTimeout(3000);
+    sleepFor(10000);
+    console.log('server execution time: 20000');
+    res.json(["Tony","Lissa","Michael","Ginger","Food"]);
+   });
+
+   function sleepFor( sleepDuration ){
+    var now = new Date().getTime();
+    while(new Date().getTime() < now + sleepDuration){  } 
+}
+app.get("/url11", (req, res, next) => {
     //sleepFor(1000);
     //console.log('server execution time:1000 ');
     //res.setTimeout(3000);
